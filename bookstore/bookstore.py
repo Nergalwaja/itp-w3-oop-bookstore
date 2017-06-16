b7cbb135c406ae7200860da7bc7d9c9f43650114
James Curbow <curbowj@uni.edu>
2017-06-16 01:26:46 +0000

    OOP Bookstore Conor, Ping, and James

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,47 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def get_books(self):
+        return self.books
+    
+    def search_books(self, author=None, title=None):
+        # import pdb
+        # pdb.set_trace()
 
+        if author is None and title is None:
+            return []
+        elif author is None and title is not None:
+            title_list = []
+            for book in self.books: 
+                if title.lower() in book.title.lower():
+                    title_list.append(book)
+            return title_list
+        elif author is not None and title is None:
+            author_list = []
+            for book in self.books: 
+                if author == book.author:
+                    author_list.append(book)
+            return author_list
+        
 
+    
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.books.append(self)
